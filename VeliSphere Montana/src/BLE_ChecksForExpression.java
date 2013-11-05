d619df5dbadb18bdd0918b5d2c8051bb638eeae1
Thorsten Meudt <thorsten@thorsten-meudt.de>
2013-11-04 22:44:53 -0800

    major bug fixed in business logic engine, it only checked for equal values and ignored operator defined in check!!!

--- VeliSphere Montana/src/BLE_ChecksForExpression.java
+++ VeliSphere Montana/src/BLE_ChecksForExpression.java
@@ -1,54 +1,71 @@
-
 import java.util.HashSet;
 import java.util.Iterator;
 import org.voltdb.SQLStmt;
 import org.voltdb.VoltProcedure;
 import org.voltdb.VoltTable;
 
-
 public class BLE_ChecksForExpression extends VoltProcedure {
-	
+
 	public final SQLStmt sqlFindTrueChecks = new SQLStmt(
-			"SELECT CHECKID FROM CHECK WHERE ENDPOINTID = ? AND PROPERTYID = ? AND CHECKVALUE = ? AND OPERATOR = ? AND EXPIRED = ?;"
-			);
+			"SELECT CHECKID, OPERATOR, CHECKVALUE FROM CHECK WHERE ENDPOINTID = ? AND PROPERTYID = ? AND EXPIRED = ?;");
 
 	public final SQLStmt sqlUpdateTrueChecks = new SQLStmt(
-			"UPDATE CHECK SET STATE = 1 WHERE CHECKID = ?;"
-			);
+			"UPDATE CHECK SET STATE = 1 WHERE CHECKID = ?;");
 
-	public VoltTable[] run( 	
-			String endpointID, 
-			String propertyID, 
-			String checkValue, 
-			String operator,
-			byte expired)
-					throws VoltAbortException {
-		voltQueueSQL( sqlFindTrueChecks, endpointID, propertyID, checkValue, operator, expired );
+	public VoltTable[] run(String endpointID, String propertyID,
+			String checkValue, byte expired)
+			throws VoltAbortException {
+		voltQueueSQL(sqlFindTrueChecks, endpointID, propertyID, expired);
 		// voltExecuteSQL();
 
 		VoltTable[] findTrueChecksResults = voltExecuteSQL();
 
-		if (findTrueChecksResults.length != 0){
+		if (findTrueChecksResults.length != 0) {
 
-			HashSet<String> trueChecksList = new HashSet<String>();			
+			HashSet<String> trueChecksList = new HashSet<String>();
 
 			VoltTable findTrueChecks = findTrueChecksResults[0];
-			while (findTrueChecks.advanceRow()){
-				trueChecksList.add(findTrueChecks.getString("CHECKID"));
+			while (findTrueChecks.advanceRow()) {
+				if (findTrueChecks.getString("OPERATOR").equals("=")
+						&& findTrueChecks.getString("CHECKVALUE").equals(
+								checkValue)) {
+					trueChecksList.add(findTrueChecks.getString("CHECKID"));
+				} else if (findTrueChecks.getString("OPERATOR").equals("!=")
+						&& findTrueChecks.getString("CHECKVALUE").equals(checkValue)==false) {
+					trueChecksList.add(findTrueChecks.getString("CHECKID"));
+				} else if (findTrueChecks.getString("OPERATOR").equals("<")
+						&& Float.parseFloat(findTrueChecks
+								.getString("CHECKVALUE")) < Float
+								.parseFloat(checkValue)) {
+					trueChecksList.add(findTrueChecks.getString("CHECKID"));
+				} else if (findTrueChecks.getString("OPERATOR").equals(">")
+						&& Float.parseFloat(findTrueChecks
+								.getString("CHECKVALUE")) > Float
+								.parseFloat(checkValue)) {
+					trueChecksList.add(findTrueChecks.getString("CHECKID"));
+				} else if (findTrueChecks.getString("OPERATOR").equals(">=")
+						&& Float.parseFloat(findTrueChecks
+								.getString("CHECKVALUE")) >= Float
+								.parseFloat(checkValue)) {
+					trueChecksList.add(findTrueChecks.getString("CHECKID"));
+				} else if (findTrueChecks.getString("OPERATOR").equals("<=")
+						&& Float.parseFloat(findTrueChecks
+								.getString("CHECKVALUE")) <= Float
+								.parseFloat(checkValue)) {
+					trueChecksList.add(findTrueChecks.getString("CHECKID"));
+				} 			
+
 			}
 
-			
 			Iterator<String> itTCL = trueChecksList.iterator();
-			while (itTCL.hasNext()){
+			while (itTCL.hasNext()) {
 				String sTR = itTCL.next();
-				voltQueueSQL( sqlUpdateTrueChecks, sTR );
+				voltQueueSQL(sqlUpdateTrueChecks, sTR);
 				voltExecuteSQL();
 			}
-			
+
 		}
 
 		return findTrueChecksResults;
 	}
 }
-
-
