f292f8ec74c93759c6a56302350635f755a47e47
Thorsten Meudt <thorsten@thorsten-meudt.de>
2013-12-24 02:09:33 -0800

    Enhanced checkpath handling to be able to store json version of checkpath diagram in checkpath table (uiObject)

---
+++ VeliSphere Montana/src/UI_SelectCheckpathForCheckpathID.java
@@ -0,0 +1,18 @@
+import org.voltdb.*;
+
+public class UI_SelectCheckpathForCheckpathID extends VoltProcedure {
+
+	
+	public final SQLStmt CheckpathForCheckpathID = new SQLStmt(" select checkpathid, uiobject from checkpath " +
+			"where checkpathid = ?;");
+		
+
+  public VoltTable[] run(
+		  String checkpathID)
+      throws VoltAbortException {
+
+          voltQueueSQL( CheckpathForCheckpathID, checkpathID );
+          return voltExecuteSQL();
+          
+      }
+}
