3474f5ea0cba63f0ed652bffd3d566f0d35dd3cf
thorsten <thorsten.meudt@connectedthingslab.com>
2014-05-17 16:05:35 -0400

    Added userID to checkpath

--- VeliSphereMontana/src/UI_SelectAllCheckpaths.java
+++ VeliSphereMontana/src/UI_SelectAllCheckpaths.java
@@ -3,12 +3,13 @@ import org.voltdb.*;
 public class UI_SelectAllCheckpaths extends VoltProcedure {
 
   public final SQLStmt selectAllCheckpaths = new SQLStmt(
-      " SELECT CHECKPATHID, CHECKPATHNAME FROM CHECKPATH ORDER BY CHECKPATHNAME;" );
+      " SELECT CHECKPATHID, CHECKPATHNAME FROM CHECKPATH WHERE OWNERID = ? ORDER BY CHECKPATHNAME;" );
 
-  public VoltTable[] run()
+  public VoltTable[] run(
+		  String userID)
       throws VoltAbortException {
 
-          voltQueueSQL( selectAllCheckpaths );
+          voltQueueSQL( selectAllCheckpaths, userID );
           return voltExecuteSQL();
 
       }
