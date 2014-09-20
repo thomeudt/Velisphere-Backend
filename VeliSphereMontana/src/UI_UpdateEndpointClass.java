24ad10831804dc15282d86a4782f26928de591e8
thorsten <thorsten.meudt@connectedthingslab.com>
2014-09-20 20:00:16 +0200

    vendor added to endpoint class

--- VeliSphereMontana/src/UI_UpdateEndpointClass.java
+++ VeliSphereMontana/src/UI_UpdateEndpointClass.java
@@ -3,15 +3,16 @@ import org.voltdb.*;
 public class UI_UpdateEndpointClass extends VoltProcedure {
 
   public final SQLStmt updateEPC = new SQLStmt(
-      " UPDATE ENDPOINTCLASS SET ENDPOINTCLASSNAME = ?, ENDPOINTCLASSIMAGEURL = ? WHERE ENDPOINTCLASSID = ?;" );
+      " UPDATE ENDPOINTCLASS SET ENDPOINTCLASSNAME = ?, ENDPOINTCLASSIMAGEURL = ?, VENDORID = ?  WHERE ENDPOINTCLASSID = ?;" );
 
   public VoltTable[] run(
 		  String endpointClassID,
 		  String endpointClassName,
-		  String endpointClassImageurl)
+		  String endpointClassImageurl, 
+		  String vendorID)
       throws VoltAbortException {
 
-          voltQueueSQL( updateEPC, endpointClassName, endpointClassImageurl, endpointClassID );
+          voltQueueSQL( updateEPC, endpointClassName, endpointClassImageurl, vendorID, endpointClassID );
           return voltExecuteSQL();
 
       }
