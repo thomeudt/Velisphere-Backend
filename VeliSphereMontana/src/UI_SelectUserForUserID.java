d98a9de05ce0f298431b8f15e574b0622ffe4fcc
thorsten <thorsten.meudt@connectedthingslab.com>
2015-01-20 08:53:31 +0100

    procedures added to find users with pub spheres

---
+++ VeliSphereMontana/src/UI_SelectUserForUserID.java
@@ -0,0 +1,20 @@
+import org.voltdb.*;
+import org.voltdb.VoltProcedure.VoltAbortException;
+
+public class UI_SelectUserForUserID extends VoltProcedure {
+
+	
+	public final SQLStmt findUserForUserID = new SQLStmt(" SELECT USERID, USERNAME, USEREMAIL "
+			+ "FROM USER " +
+			"WHERE USERID = ? ORDER BY USERNAME;");
+		
+
+  public VoltTable[] run(
+		  String userID)
+      throws VoltAbortException {
+
+          voltQueueSQL( findUserForUserID, userID );
+          return voltExecuteSQL();
+          
+      }
+}
